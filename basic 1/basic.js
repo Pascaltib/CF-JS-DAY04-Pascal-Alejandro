d905f06e4922b13cd6849f4993d1156eb4eeb391
Pascal Tiberghien <pascaltib@gmail.com>
2020-09-23 11:05:12 +0200

    Basic exercise 01 and 02

--- basic 1/basic.js
+++ basic 1/basic.js
@@ -1,12 +1,24 @@
 function getValue(){
     var x = document.getElementById('name').value;
-    //document.getElementById('result').innerHTML = x;
-    console.log(x);
+    var select = document.getElementById('select').value;
+    if(select == "IT"){
+    	document.getElementById("result").style.backgroundColor = "purple";
+    }
+    else if (select == "Hospitality"){
+    	document.getElementById("result").style.backgroundColor = "yellow";
+    }
+    else{
+    	document.getElementById("result").style.backgroundColor = "lightgrey";
+    }
+    if(x.length>5){
+    	document.getElementById("result").style.color = "green";
+    }
+    else{
+    	document.getElementById("result").style.color = "red";
+    }
+    var y = document.getElementById('lastName').value;
+    var z = document.getElementById('age').value;
+    document.getElementById('result').innerHTML = `Name: ${x} <br> Last Name: ${y} <br> Age: ${z} `;
 }
 document.getElementById('button').addEventListener('click', getValue, false);
 
-/*function test(){
-	var select = document.getElementById('select').value;
-	console.log(select);
-}
-document.getElementById("submit").addEventListener("click",test,false);*/
