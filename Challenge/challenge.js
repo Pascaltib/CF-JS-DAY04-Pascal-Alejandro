27fd9650c3c08e77335287cc1b834711b63db637
Alejandro <alejandro.rodriguez95@gmail.com>
2020-09-23 14:48:00 +0200

    half-finished challenge

--- Challenge/challenge.js
+++ Challenge/challenge.js
@@ -12,25 +12,37 @@ class Quiz {
         this.correctAnswer = rightAnswer;
     }
 }
+var i = 0;
+var arr = [];
 
 
-var quiz1 = new Quiz("","","","","");
 
-function submitQuiz(quiz) {
-    quiz.question = document.getElementById('query').value;
-    quiz.answer1 = document.getElementById('answer1').value;
-    quiz.answer2 = document.getElementById('answer2').value;
-    quiz.answer3 = document.getElementById('answer3').value;
-    quiz.correctAnswer = document.getElementById('correct').value;
-    return quiz;
+function submitQuiz() {
+    var quiz1 = new Quiz("", "", "", "", "");
+    quiz1.question = document.getElementById('query').value;
+    quiz1.answer1 = document.getElementById('answer1').value;
+    quiz1.answer2 = document.getElementById('answer2').value;
+    quiz1.answer3 = document.getElementById('answer3').value;
+    quiz1.correctAnswer = document.getElementById('correct').value;
+    arr.push(quiz1);
 }
 
-document.getElementById('send').addEventListener('click', 
-function(){submitQuiz(quiz1); 
-document.getElementById('qSection2').innerHTML=quiz1.question;
-document.getElementById('a1Section2').innerHTML=quiz1.answer1;
-document.getElementById('a2Section2').innerHTML=quiz1.answer2;
-document.getElementById('a3Section2').innerHTML=quiz1.answer3;
-document.getElementById('a4Section2').innerHTML=quiz1.correctAnswer;
-}, false);
 
+//part that fills section 2
+document.getElementById('send').addEventListener('click',
+    function () {
+        submitQuiz();
+        i++;
+    }, false);
+
+
+function randomQuestion() {
+    var x = Math.floor(Math.random() * arr.length);
+    document.getElementById('qSection2').innerHTML = arr[x].question;
+    document.getElementById('a1Section2').innerHTML = arr[x].answer1;
+    document.getElementById('a2Section2').innerHTML = arr[x].answer2;
+    document.getElementById('a3Section2').innerHTML = arr[x].answer3;
+    document.getElementById('a4Section2').innerHTML = arr[x].correctAnswer;
+    console.log(x);
+}
+document.getElementById('random').addEventListener('click', randomQuestion, false);
